6984e32413d3a04c1c05e87f4c53919cd43c72b3
PiotrKozlowski11 <85372826+PiotrKozlowski11@users.noreply.github.com>
2021-10-30 22:33:14 +0200

    Implement Interfaces for services. Closes #5

---
+++ pet-clinic-data/src/main/java/org/kozlowski/springframework/services/VetService.java
@@ -0,0 +1,14 @@
+package org.kozlowski.springframework.services;
+
+import org.kozlowski.springframework.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
