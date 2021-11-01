e4511c24d45e3bc2641e48ed86bbeb615efc5433
PiotrKozlowski11 <85372826+PiotrKozlowski11@users.noreply.github.com>
2021-11-01 20:36:58 +0100

    Implementing Map Based Service. Closes #10

---
+++ pet-clinic-data/src/main/java/org/kozlowski/springframework/services/map/PetServiceMap.java
@@ -0,0 +1,36 @@
+package org.kozlowski.springframework.services.map;
+
+import org.kozlowski.springframework.model.Pet;
+import org.kozlowski.springframework.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+
+}
---
+++ pet-clinic-data/src/main/java/org/kozlowski/springframework/services/map/VetServiceMap.java
@@ -0,0 +1,36 @@
+package org.kozlowski.springframework.services.map;
+
+import org.kozlowski.springframework.model.Vet;
+import org.kozlowski.springframework.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+
+    }
+
+
+}
