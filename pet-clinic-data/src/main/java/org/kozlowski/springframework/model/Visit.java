3a1b3cf7face0c607e949478a91ef481a5795e38
PiotrKozlowski11 <85372826+PiotrKozlowski11@users.noreply.github.com>
2021-11-04 18:54:41 +0100

    Cleaned up code, optimized imports

--- pet-clinic-data/src/main/java/org/kozlowski/springframework/model/Visit.java
+++ pet-clinic-data/src/main/java/org/kozlowski/springframework/model/Visit.java
@@ -2,7 +2,7 @@ package org.kozlowski.springframework.model;
 
 import java.time.LocalDate;
 
-public class Visit extends BaseEntity{
+public class Visit extends BaseEntity {
     private LocalDate date;
     private String description;
     private Pet pet;
