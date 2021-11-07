f8233b58e41587f541f013b4f254b393d552a9a1
PiotrKozlowski11 <85372826+PiotrKozlowski11@users.noreply.github.com>
2021-11-07 18:04:24 +0100

    Implemented JPA Visit Service, added loader data. Closes #47

--- pet-clinic-data/src/main/java/org/kozlowski/springframework/services/VisitService.java
+++ pet-clinic-data/src/main/java/org/kozlowski/springframework/services/VisitService.java
@@ -1,7 +1,6 @@
 package org.kozlowski.springframework.services;
 
 import org.kozlowski.springframework.model.Visit;
-import org.kozlowski.springframework.services.CrudService;
 
-public interface VisitService extends CrudService<Visit,Long> {
+public interface VisitService extends CrudService<Visit, Long> {
 }
--- pet-clinic-data/src/main/java/org/kozlowski/springframework/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/org/kozlowski/springframework/services/map/PetMapService.java
@@ -1,13 +1,13 @@
 package org.kozlowski.springframework.services.map;
 
 import org.kozlowski.springframework.model.Pet;
-import org.kozlowski.springframework.services.CrudService;
+import org.kozlowski.springframework.services.PetService;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
-public class PetMapService extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
